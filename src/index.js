0217b0594995165f6b7eb6214a102b17508042e5
markmead <itsmarkmead@gmail.com>
2020-01-18 13:16:41 +0000

    switch to clientX & clientY pos

--- src/index.js
+++ src/index.js
@@ -38,13 +38,13 @@ Cursor.prototype.moveCursor = function() {
   }
 
   document.addEventListener("mousemove", function(event) {
-    const { pageX, pageY } = event;
-    CURSOR.style.left = `${pageX - CURSOR.offsetWidth / 2}px`;
-    CURSOR.style.top = `${pageY - CURSOR.offsetHeight / 2}px`;
+    const { clientX, clientY } = event;
+    CURSOR.style.left = `${clientX - CURSOR.offsetWidth / 2}px`;
+    CURSOR.style.top = `${clientY - CURSOR.offsetHeight / 2}px`;
 
     if (SECOND_CURSOR) {
-      SECOND_CURSOR.style.left = `${pageX - SECOND_CURSOR.offsetWidth / 2}px`;
-      SECOND_CURSOR.style.top = `${pageY - SECOND_CURSOR.offsetHeight / 2}px`;
+      SECOND_CURSOR.style.left = `${clientX - SECOND_CURSOR.offsetWidth / 2}px`;
+      SECOND_CURSOR.style.top = `${clientY - SECOND_CURSOR.offsetHeight / 2}px`;
     }
   });
 };

0c810f2bac932d893a75259c20f38ccba81f0147
markmead <itsmarkmead@gmail.com>
2020-07-05 21:16:33 +0100

    Change default cursor name to `custom-cursor`

--- README.md
+++ README.md
@@ -18,15 +18,15 @@ Create your own custom cursor with minimal JavaScript
 To get the cursor working, add this to your javascript file
 
 ```js
-import Cursor from "custom-cursor";
+import Cursor from 'custom-cursor'
 
-new Cursor({}).mount();
+new Cursor({}).mount()
 ```
 
 ### Default Cursor
 
 ```html
-<div id="_cursor" class="_cursor" style="position: absolute; pointer-events: none;"></div>
+<div id="custom-cursor" class="custom-cursor" style="position: absolute; pointer-events: none;"></div>
 ```
 
 ## Options
@@ -40,9 +40,9 @@ There's 3 options available and they are all optional:
 ```js
 const customCursor = new Cursor({
   secondCursor: true, // default = false
-  hoverTargets: [".link-button", "#hero-text", "p"], // default = null
-  browserCursor: false // default = true
-});
+  hoverTargets: ['.link-button', '#hero-text', 'p'], // default = null
+  browserCursor: false, // default = true
+})
 ```
 
 ### secondCursor
@@ -50,7 +50,7 @@ const customCursor = new Cursor({
 This adds a second cursor element to the page with the following markup:
 
 ```html
-<div id="_cursor-second" class="_cursor-second" style="position: absolute; pointer-events: none;"></div>
+<div id="custom-cursor-second" class="custom-cursor-second" style="position: absolute; pointer-events: none;"></div>
 ```
 
 ### hoverTargets
@@ -61,30 +61,30 @@ The name of the element in the array will be given as a class name to the body e
 
 ```js
 new Cursor({
-  hoverTargets: [".link-button", "#hero-text", "p"]
-});
+  hoverTargets: ['.link-button', '#hero-text', 'p'],
+})
 ```
 
 Will result in:
 
 ```html
-<body class="_cursor-hover--link-button"></body>
-<body class="_cursor-hover--hero-text"></body>
-<body class="_cursor-hover--p"></body>
+<body class="custom-cursor-hover--link-button"></body>
+<body class="custom-cursor-hover--hero-text"></body>
+<body class="custom-cursor-hover--p"></body>
 ```
 
 The class is applied to the body so that in CSS you can easily add different styles per hover, like so:
 
 ```css
-._cursor-hover--link-button ._cursor {
+.custom-cursor-hover--link-button .custom-cursor {
   background: red;
 }
 
-._cursor-hover--hero-text ._cursor-second {
+.custom-cursor-hover--hero-text .custom-cursor-second {
   background: green;
 }
 
-._cursor-hover--hero-text ._cursor {
+.custom-cursor-hover--hero-text .custom-cursor {
   background: green;
 }
 ```
--- src/index.js
+++ src/index.js
@@ -1,89 +1,83 @@
 export default function Cursor(data) {
-  this.name = "_cursor";
-  this.hoverTargets = data.hoverTargets || false;
-  this.browserCursor = data.browserCursor === false ? false : true;
-  this.secondCursor = data.secondCursor || false;
+  this.name = 'custom-cursor'
+  this.hoverTargets = data.hoverTargets || false
+  this.browserCursor = data.browserCursor === false ? false : true
+  this.secondCursor = data.secondCursor || false
 }
 
-Cursor.prototype.buildCursor = function() {
-  const HTML = document.getElementsByTagName("html")[0];
-  const BODY = document.getElementsByTagName("body")[0];
-  const CURSOR = document.createElement("div");
-  const STYLE = "position: absolute; pointer-events: none;";
+Cursor.prototype.buildCursor = function () {
+  const HTML = document.getElementsByTagName('html')[0]
+  const BODY = document.getElementsByTagName('body')[0]
+  const CURSOR = document.createElement('div')
+  const STYLE = 'position: absolute; pointer-events: none;'
 
-  CURSOR.setAttribute("id", this.name);
-  CURSOR.setAttribute("class", this.name);
-  CURSOR.style = STYLE;
-  BODY.append(CURSOR);
+  CURSOR.setAttribute('id', this.name)
+  CURSOR.setAttribute('class', this.name)
+  CURSOR.style = STYLE
+  BODY.append(CURSOR)
 
   if (this.secondCursor) {
-    const SECOND_CURSOR = document.createElement("div");
-    SECOND_CURSOR.setAttribute("id", `${this.name}-second`);
-    SECOND_CURSOR.setAttribute("class", `${this.name}-second`);
-    SECOND_CURSOR.style = STYLE;
-    BODY.append(SECOND_CURSOR);
+    const SECOND_CURSOR = document.createElement('div')
+    SECOND_CURSOR.setAttribute('id', `${this.name}-second`)
+    SECOND_CURSOR.setAttribute('class', `${this.name}-second`)
+    SECOND_CURSOR.style = STYLE
+    BODY.append(SECOND_CURSOR)
   }
 
   if (!this.browserCursor) {
-    HTML.style.cursor = "none";
+    HTML.style.cursor = 'none'
   }
-};
+}
 
-Cursor.prototype.moveCursor = function() {
-  const CURSOR = document.querySelector("#_cursor");
-  let SECOND_CURSOR;
+Cursor.prototype.moveCursor = function () {
+  const CURSOR = document.querySelector(`#${this.name}`)
+  let SECOND_CURSOR
 
   if (this.secondCursor) {
-    SECOND_CURSOR = document.querySelector("#_cursor-second");
+    SECOND_CURSOR = document.querySelector(`#${this.name}-second`)
   }
 
-  document.addEventListener("mousemove", function(event) {
-    const { clientX, clientY } = event;
-    CURSOR.style.left = `${clientX - CURSOR.offsetWidth / 2}px`;
-    CURSOR.style.top = `${clientY - CURSOR.offsetHeight / 2}px`;
+  document.addEventListener('mousemove', function (event) {
+    const { clientX, clientY } = event
+    CURSOR.style.left = `${clientX - CURSOR.offsetWidth / 2}px`
+    CURSOR.style.top = `${clientY - CURSOR.offsetHeight / 2}px`
 
     if (SECOND_CURSOR) {
-      SECOND_CURSOR.style.left = `${clientX - SECOND_CURSOR.offsetWidth / 2}px`;
-      SECOND_CURSOR.style.top = `${clientY - SECOND_CURSOR.offsetHeight / 2}px`;
+      SECOND_CURSOR.style.left = `${clientX - SECOND_CURSOR.offsetWidth / 2}px`
+      SECOND_CURSOR.style.top = `${clientY - SECOND_CURSOR.offsetHeight / 2}px`
     }
-  });
-};
+  })
+}
 
-Cursor.prototype.cursorStatus = function() {
+Cursor.prototype.cursorStatus = function () {
   if (!this.hoverTargets) {
-    return;
+    return
   }
 
   for (const hoverTarget of this.hoverTargets) {
-    const hoverTargetsArray = [...document.querySelectorAll(hoverTarget)];
+    const hoverTargetsArray = [...document.querySelectorAll(hoverTarget)]
 
     for (const _hoverTarget of hoverTargetsArray) {
-      _hoverTarget.addEventListener(
-        "mouseover",
-        this.cursorHover.bind(this, hoverTarget)
-      );
-      _hoverTarget.addEventListener(
-        "mouseleave",
-        this.cursorLeave.bind(this, hoverTarget)
-      );
+      _hoverTarget.addEventListener('mouseover', this.cursorHover.bind(this, hoverTarget))
+      _hoverTarget.addEventListener('mouseleave', this.cursorLeave.bind(this, hoverTarget))
     }
   }
-};
+}
 
-Cursor.prototype.cursorHover = function(hoverTarget) {
-  const BODY = document.getElementsByTagName("body")[0];
-  const hoverTargetName = hoverTarget.replace(/[.#!]/g, "");
-  BODY.classList.add(`_cursor-hover--${hoverTargetName}`);
-};
+Cursor.prototype.cursorHover = function (hoverTarget) {
+  const BODY = document.getElementsByTagName('body')[0]
+  const hoverTargetName = hoverTarget.replace(/[.#!]/g, '')
+  BODY.classList.add(`${this.name}-hover--${hoverTargetName}`)
+}
 
-Cursor.prototype.cursorLeave = function(hoverTarget) {
-  const BODY = document.getElementsByTagName("body")[0];
-  const hoverTargetName = hoverTarget.replace(/[.#!]/g, "");
-  BODY.classList.remove(`_cursor-hover--${hoverTargetName}`);
-};
+Cursor.prototype.cursorLeave = function (hoverTarget) {
+  const BODY = document.getElementsByTagName('body')[0]
+  const hoverTargetName = hoverTarget.replace(/[.#!]/g, '')
+  BODY.classList.remove(`${this.name}-hover--${hoverTargetName}`)
+}
 
-Cursor.prototype.mount = function() {
-  this.buildCursor();
-  this.moveCursor();
-  this.cursorStatus();
-};
+Cursor.prototype.mount = function () {
+  this.buildCursor()
+  this.moveCursor()
+  this.cursorStatus()
+}

db397da54f9605cca0fc66e7091ec602e2c0c0e1
Mark Mead <itsmarkmead@gmail.com>
2022-07-22 08:58:34 +0100

    Update - ES Build
    Got a working CDN version with this as well! 🎉

---
+++ builds/module.js
@@ -0,0 +1,3 @@
+import Cursor from '../src/index.js'
+
+export default Cursor
--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,74 @@
-interface Data {
-  count?: number;
-  targets?: string[] | boolean;
+interface CursorInterface {
+  count?: number
+  targets?: string[] | boolean
 }
 
 export default class Cursor {
-  count: number;
-  targets: string[] | boolean;
+  count: number
+  targets: string[] | boolean
 
-  style = "position: fixed; pointer-events: none;";
-  body = document.querySelector("body") as HTMLBodyElement;
+  style = 'position: fixed; pointer-events: none;'
+  body = document.querySelector('body') as HTMLBodyElement
 
-  constructor(private data: Data) {
-    this.count = this.data.count || 1;
-    this.targets = this.data.targets || false;
+  constructor(private data: CursorInterface) {
+    this.count = this.data.count || 1
+    this.targets = this.data.targets || false
 
-    this.init();
-    this.move();
-    this.status();
+    this.init()
+    this.move()
+    this.status()
   }
 
   private init(): void {
-    const cursors = new Array(this.count).fill(0);
+    const cursors = new Array(this.count).fill(0)
 
     cursors.forEach((_, index) => {
-      const cursor: HTMLDivElement = document.createElement("div");
+      const cursor: HTMLDivElement = document.createElement('div')
 
-      this.create(cursor, index);
-    });
+      this.create(cursor, index)
+    })
   }
 
   private create(cursor: HTMLDivElement, index: number): void {
-    cursor.setAttribute("data-cursor", `${index}`);
-    cursor.setAttribute("style", this.style);
+    cursor.setAttribute('data-cursor', `${index}`)
+    cursor.setAttribute('style', this.style)
 
-    this.body.append(cursor);
+    this.body.append(cursor)
   }
 
   private move(): void {
     const cursors: NodeListOf<Element> =
-      document.querySelectorAll("[data-cursor]");
+      document.querySelectorAll('[data-cursor]')
 
-    document.addEventListener("mousemove", (event) => {
-      const { clientX, clientY } = event;
+    document.addEventListener('mousemove', (event) => {
+      const { clientX, clientY } = event
 
       cursors.forEach((cursor) =>
         this.position(cursor as HTMLDivElement, clientX, clientY)
-      );
-    });
+      )
+    })
   }
 
   private position(cursor: HTMLDivElement, x: number, y: number): void {
-    cursor.style.transform = `translate3d(calc(${x}px - 50%), calc(${y}px - 50%), 0)`;
+    cursor.style.transform = `translate3d(calc(${x}px - 50%), calc(${y}px - 50%), 0)`
   }
 
   private status(): void {
     if (this.targets instanceof Array) {
       for (const target of this.targets) {
-        const targetEls: NodeListOf<Element> =
-          document.querySelectorAll(target);
+        const targetEls: NodeListOf<Element> = document.querySelectorAll(target)
 
         for (const el of targetEls) {
-          el.addEventListener("mouseover", this.hover.bind(this, target));
-          el.addEventListener("mouseleave", this.hover.bind(this, target));
+          el.addEventListener('mouseover', this.hover.bind(this, target))
+          el.addEventListener('mouseleave', this.hover.bind(this, target))
         }
       }
     }
   }
 
   private hover(hoverTarget: string): void {
-    const name = hoverTarget.replace(/[.#!]/g, "");
+    const name = hoverTarget.replace(/[.#!]/g, '')
 
-    this.body.classList.toggle(`cursor-hover--${name}`);
+    this.body.classList.toggle(`cursor-hover--${name}`)
   }
 }
